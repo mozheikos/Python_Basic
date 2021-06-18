c306f18be1e03687ef4764220ee44235cc157296
Stanislav_Mozheiko <mozheiko.stanislav@yandex.ru>
2021-06-18 19:51:00 +0300

    Вот теперь точно самый правильный вариант

--- Mozheiko_Stanislav_Lesson2_task5.py
+++ Mozheiko_Stanislav_Lesson2_task5.py
@@ -1,11 +1,15 @@
 def format_string_task_a(some_list):
     new_l = []
+    some_prices = []
     for i in range(0, len(some_list), 1):
-        rub = int(some_list[i] // 1)
-        kop = int((some_list[i] % 1)*100)
-        st = f'{rub} руб {kop:02d} коп'
+        some_prices.append(str(some_list[i]))
+        some_price = some_prices[i].split('.')
+        rub = some_price[0]
+        kop = (some_price[1])
+        if len(kop) < 2:
+            kop = kop + '0'
+        st = f'{rub} руб {kop} коп'
         new_l.append(st)
-    new_l = ', '.join(new_l)
     return new_l
 
 
