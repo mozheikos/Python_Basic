57fa4c4a185b0845fa05970d38024e1b4a1c7367
mozheikos <85384650+mozheikos@users.noreply.github.com>
2021-06-21 08:05:09 +0300

    Update Mozheiko_Stanislav_Lesson3_task2.py
    чуть доработал, было не совсем правильно

--- Mozheiko_Stanislav_Lesson3_task2.py
+++ Mozheiko_Stanislav_Lesson3_task2.py
@@ -7,7 +7,10 @@ def num_translate_adv(key):
 
 
 while True:
-    word = num_translate_adv(input('введите слово для перевода или "exit" для выхода из программы\n'))
+    word_ = input('введите слово для перевода или "exit" для выхода из программы\n')
+    word = num_translate_adv(word_)
+    if word_[:1].isupper():
+      word = word.capitalize()
     if word != 1:
         print(word)
     else:
